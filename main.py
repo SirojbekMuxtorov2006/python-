c58b3e5ca28fbbda2bbdada519892e131751545c
SirojbekMuxtorov2006 <sirojbekmuxtorov9@gmail.com>
2025-12-03 14:28:25 +0500

    prakita  pytho lesso

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 
+# ism = 'Sirojbek'
+# yosh = 20
+# print(ism)
+# print(yosh)
+
 ism = 'Sirojbek'
-yosh = 20
 print(ism)
-print(yosh)
+ism = 'Yusufbek'
+print(ism)
+
+a = 6
+b = 7
+c = (6+7) ** 2
+print(c)
+
+ism_sharif = 'Sirojbek Muxtorov'
+print(ism_sharif)
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radius:", radius, "ga teng aylananing yuzi=", aylana_yuzi)
