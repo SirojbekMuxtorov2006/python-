ef6d70aebf6dfab6556c2e0aeb7171ad39606625
SirojbekMuxtorov2006 <sirojbekmuxtorov9@gmail.com>
2025-12-05 15:54:34 +0500

    staring

--- 2-dars/index.py
+++ 2-dars/index.py
@@ -11,4 +11,15 @@ print(type(ism))
 print(type(viloyat))
 print(type(shahar))
 
+# print('Hello World')
+# print('Hello \tWorld')
+# print('Hello \nWorld')
+
+ism = 'James'
+familiya = 'bond'
+ism_sharif = f"{ism} {familiya}"
+print(ism_sharif.upper())
+print(ism_sharif.lower())
+print(ism_sharif.title())
+print(ism_sharif.capitalize())
 
